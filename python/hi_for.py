b7cadc8e37f9de90631e79181913c57f5e86f97e
JongHun <kj001117@gmail.com>
2023-01-03 16:22:31 +0900

    1

---
+++ python/hi_for.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    print(i)
+
+#0부터 10까지 1씩 증가
+for i in range(0, 10, 1):   
+    print(i)
+
+#0부터 10까지 1씩 증가, 가로 출력
+for i in range(0, 10, 1):   
+    print(i, end=''    )
+
+print('\n')
+
+ch='string'
+for i in ch:
+    print(i)
